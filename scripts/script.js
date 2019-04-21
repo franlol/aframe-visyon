76c8fa126128caaa80b9ee6fd5a02f69f38daa45
franlol <fraaaaan@gmail.com>
2019-04-21 23:44:29 +0200

    Adding 360 vids. Some logic left

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 // Play / Pause handler
-AFRAME.registerComponent("play-pause", {
+AFRAME.registerComponent("play-pause-2d", {
     init: function () {
-        let video = document.querySelector("#video-2d");
-        let controls = document.querySelector("#controls");
+        const video = document.querySelector("#video-2d");
+        const controls = document.querySelector("#controls-2d");
 
         this.el.addEventListener("click", function () {
             if (video.paused) {
@@ -10,6 +10,31 @@ AFRAME.registerComponent("play-pause", {
                 controls.setAttribute("src", "#pause");
             } else {
                 video.pause();
+                controls.setAttribute("src", "#play")
+            }
+        });
+    }
+});
+
+// Play / Pause handler (360)
+AFRAME.registerComponent("play-pause-360", {
+    init: function () {
+        const video = document.querySelector("#video-360");
+        const controls = document.querySelector("#controls-360");
+        const plane = document.querySelector("a-plane");
+        const sky = document.querySelector("a-sky");
+        
+        this.el.addEventListener("click", function () {
+            console.log(plane, sky)
+            if (video.paused) {
+                video.play();
+                plane.setAttribute("visible", false);
+                sky.setAttribute("visible", false);
+                controls.setAttribute("src", "#pause");
+            } else {
+                video.pause();
+                plane.setAttribute("visible", true);
+                sky.setAttribute("visible", true);
                 controls.setAttribute("src", "#play")
             }
         });
