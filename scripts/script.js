7091c98ade68272cba6dd6d7a506924c0f1da3ba
franlol <fraaaaan@gmail.com>
2019-04-21 22:05:43 +0200

    Adding play-pause component

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,17 @@
-'use strict'
+// Play / Pause handler
+AFRAME.registerComponent("play-pause", {
+    init: function () {
+        let video = document.querySelector("#video-2d");
+        let controls = document.querySelector("#controls");
 
-const main = () => {
-    // my custom js
-}
-
-window.addEventListener("load", main);
+        this.el.addEventListener("click", function () {
+            if (video.paused) {
+                video.play();
+                controls.setAttribute("src", "#pause");
+            } else {
+                video.pause();
+                controls.setAttribute("src", "#play")
+            }
+        });
+    }
+});
