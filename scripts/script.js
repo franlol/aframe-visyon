5e431f6408e053277774e80ffb3d91fc49305367
franlol <fraaaaan@gmail.com>
2019-04-22 12:48:06 +0200

    Adding 'show-image-360' component to Backgrounds-Links, hidding 360vid and showing 360 backgorund

--- scripts/script.js
+++ scripts/script.js
@@ -23,20 +23,50 @@ AFRAME.registerComponent("play-pause-360", {
         const controls = document.querySelector("#controls-360");
         const plane = document.querySelector("a-plane");
         const sky = document.querySelector("a-sky");
-        
+        const sphere = document.querySelector("#videosphere");
+
         this.el.addEventListener("click", function () {
-            console.log(plane, sky)
             if (video.paused) {
                 video.play();
+                sphere.setAttribute("visible", true);
                 plane.setAttribute("visible", false);
                 sky.setAttribute("visible", false);
                 controls.setAttribute("src", "#pause");
             } else {
                 video.pause();
-                plane.setAttribute("visible", true);
-                sky.setAttribute("visible", true);
-                controls.setAttribute("src", "#play")
+                // sphere.setAttribute("visible", false);
+                // plane.setAttribute("visible", true);
+                // sky.setAttribute("visible", true);
+                controls.setAttribute("src", "#play");
             }
         });
     }
+});
+
+// Show image 360 and hanble 360-video
+AFRAME.registerComponent("show-image-360", {
+
+    init: function() {
+        const video = document.querySelector("#video-360");
+        const sky = document.querySelector("a-sky");
+        const plane = document.querySelector("a-plane");
+        const sphere = document.querySelector("#videosphere");
+        const controls = document.querySelector("#controls-360");
+
+        this.el.addEventListener("click", function() {
+            sky.setAttribute("color", "white");
+            sky.setAttribute("visible", true);
+            sphere.setAttribute("visible", false);
+            plane.setAttribute("visible", false);
+            if (!video.paused) {
+                video.pause();
+                controls.setAttribute("src", "#play");
+            }
+        });
+        // event-set__click-sky-color="_event: click; _target: #sky; color: white;"
+        // event-set__click-plane="_event: click; _target: #plane; visible: false;"
+        // event-set__click-sphere="_event: click; _target: #videosphere; visible: false;"
+        // event-set__click-sky="_event: click; _target: #sky; visible: true;">
+    }
+
 });
