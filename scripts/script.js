0be118c7381b671d91612f109ba3ef8f89079b0a
franlol <fraaaaan@gmail.com>
2019-04-22 14:54:29 +0200

    Links for 360 done and testeds

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,38 @@
+'use strict'
+
+
+// 2d Vid Play handler
+AFRAME.registerComponent("play-2d", {
+    init: function () {
+        const video = document.querySelector("#video-2d");
+        const controls = document.querySelector("#controls-2d");
+
+        this.el.addEventListener("click", () => {
+            video.play();
+            controls.setAttribute("src", "#pause");
+        });
+    }
+});
+
+// 360 Vid Play handler
+AFRAME.registerComponent("play-360", {
+    init: function () {
+        const video = document.querySelector("#video-360");
+        const controls = document.querySelector("#controls-360");
+        const plane = document.querySelector("#plane-1");
+        const sky = document.querySelector("a-sky");
+        const sphere = document.querySelector("#videosphere");
+
+        this.el.addEventListener("click", () => {
+            video.play();
+            sphere.setAttribute("visible", true);
+            plane.setAttribute("visible", false);
+            sky.setAttribute("visible", false);
+            controls.setAttribute("src", "#pause");
+        });
+    }
+});
+
 // Play / Pause handler
 AFRAME.registerComponent("play-pause-2d", {
     init: function () {
@@ -21,7 +56,7 @@ AFRAME.registerComponent("play-pause-360", {
     init: function () {
         const video = document.querySelector("#video-360");
         const controls = document.querySelector("#controls-360");
-        const plane = document.querySelector("a-plane");
+        const plane = document.querySelector("#plane-1");
         const sky = document.querySelector("a-sky");
         const sphere = document.querySelector("#videosphere");
 
@@ -34,26 +69,23 @@ AFRAME.registerComponent("play-pause-360", {
                 controls.setAttribute("src", "#pause");
             } else {
                 video.pause();
-                // sphere.setAttribute("visible", false);
-                // plane.setAttribute("visible", true);
-                // sky.setAttribute("visible", true);
                 controls.setAttribute("src", "#play");
             }
         });
     }
 });
 
-// Show image 360 and hanble 360-video
+// Show image 360 and hide 360-video
 AFRAME.registerComponent("show-image-360", {
 
-    init: function() {
+    init: function () {
         const video = document.querySelector("#video-360");
         const sky = document.querySelector("a-sky");
-        const plane = document.querySelector("a-plane");
+        const plane = document.querySelector("#plane-1");
         const sphere = document.querySelector("#videosphere");
         const controls = document.querySelector("#controls-360");
 
-        this.el.addEventListener("click", function() {
+        this.el.addEventListener("click", () => {
             sky.setAttribute("color", "white");
             sky.setAttribute("visible", true);
             sphere.setAttribute("visible", false);
@@ -63,6 +95,8 @@ AFRAME.registerComponent("show-image-360", {
                 controls.setAttribute("src", "#play");
             }
         });
+
+        // This component replace following events in each Background Links entities:
         // event-set__click-sky-color="_event: click; _target: #sky; color: white;"
         // event-set__click-plane="_event: click; _target: #plane; visible: false;"
         // event-set__click-sphere="_event: click; _target: #videosphere; visible: false;"
